29c6155eb5cce3f4ecf937824ddefee2b42ca15a
yukairex <wyktju@gmail.com>
2018-05-19 19:51:20 -0400

    add the 04 dapp

--- 04_AreYouKilledByThanos/contract.js
+++ 04_AreYouKilledByThanos/contract.js
@@ -10,44 +10,53 @@ var ThanosContract = function () {
     LocalContractStorage.defineMapProperties(this, {
       isKilled: null
     })
+    LocalContractStorage.defineProperty(this,"owner")
   };
 
 
   ThanosContract.prototype = {
     
     init: function () {
-        
+        this.owner = Blockchain.transaction.from;
       },
+
+    
+    getStatus: function(address) {
+        return this.isKilled.get(address);
+    },
 
     check: function() {
         var from = Blockchain.transaction.from;
 
         if (this.isKilled.get(from)) {
             // already checked
+            throw new Error("your address has already been checked")
         }
         
         // DO something with from
-
-        // half return killed
-        this.isKilled.set(from,1);
-    
-
-        // half return saved
-        this.isKilled.set(from,2);
+        var number = Math.random();
+        
+        if (number>0.5){
+            // half return killed  
+            this.isKilled.set(from,1);
+        }
+        else {
+             // half return saved
+            this.isKilled.set(from,0);
+        }
     },
 
 
     save: function() {
         var from = Blockchain.transaction.from;
         
-        if (this.isKilled.get(from)===1) {
+        if (this.isKilled.get(from)==1) {
             // you address will be killed
             var value = Blockchain.transaction.value;
 
-            if (value>100000)
+            if (value>=0.01)
             {
-                this.isKilled.set(from,2);
-                return 1;
+                this.isKilled.set(from,0);
             }
             else {
                 throw new Error("not enough NAS to save you!")
@@ -55,11 +64,13 @@ var ThanosContract = function () {
         }
 
         else {
-            return 0;
-        }
-        
+            throw new Error("you are not killed, no need to save")
+        }     
     }
 
 
-  }
+}
 
+
+  
+module.exports = ThanosContract;
